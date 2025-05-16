798622436d53541588919d7bbd9e69c3131efb3d
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-16 20:45:24 +0530

    created repository

---
+++ src/main/java/com/fluxCart/identityReconciliation/repository/ContactRepository.java
@@ -0,0 +1,13 @@
+package com.fluxCart.identityReconciliation.repository;
+
+import com.fluxCart.identityReconciliation.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+    Contact findByPhone(String phone);
+    Contact findByEmail(String email);
+}
