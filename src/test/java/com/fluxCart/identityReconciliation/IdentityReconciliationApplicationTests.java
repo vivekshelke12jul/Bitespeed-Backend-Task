7ee89f892846ccf6f362f8242bf496b95ae10c1d
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-16 19:57:08 +0530

    first commit

---
+++ src/test/java/com/fluxCart/identityReconciliation/IdentityReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxCart.identityReconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
