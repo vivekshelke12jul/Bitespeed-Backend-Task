a78cdc681bd1d8108dfaa31ab766cc294cb3817d
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-17 20:37:54 +0530

    created controller class

---
+++ src/main/java/com/fluxCart/identityReconciliation/controller/ContactController.java
@@ -0,0 +1,20 @@
+package com.fluxCart.identityReconciliation.controller;
+
+import com.fluxCart.identityReconciliation.payload.request.ContactRequest;
+import com.fluxCart.identityReconciliation.payload.response.ConsolidatedContactResponse;
+import com.fluxCart.identityReconciliation.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping("/identify")
+    public ConsolidatedContactResponse identifyContact(ContactRequest req) {
+        return contactService.consolidateContacts(req);
+    }
+}
